83de65c60a1bf0052d658664bc00ed780bdd334e
seom98 <seo_m98@naver.com>
2023-11-10 11:01:45 +0900

    새로고침하면 나오게

--- SSAFIT/src/components/video/VideoList.vue
+++ SSAFIT/src/components/video/VideoList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h4>검색 결과</h4>
-        <button @click="store.getvideoList">전체</button>
+        <!-- <button @click="store.getvideoList">전체</button> -->
         <ul>
             <VideoListItem v-for="video in store.videoList" :key="video.url" :video="video" />
         </ul>
@@ -9,9 +9,14 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import { usevideoStore } from '@/stores/video'
 import VideoListItem from './VideoListItem.vue';
 const store = usevideoStore()
+
+onMounted(() => {
+    store.getvideoList();
+});
 </script>
 
 <style  scoped></style>
--- SSAFIT/src/stores/video.js
+++ SSAFIT/src/stores/video.js
@@ -14,27 +14,6 @@ export const usevideoStore = defineStore("video", () => {
     });
   };
 
-  //게시글 등록
-  // const createvideo = function (video) {
-  //   axios({
-  //     url: REST_video_API,
-  //     method: "POST",
-  //     //아래꺼 없어도 알아서 보내더라 axios 쵝오~
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     data: video,
-  //   })
-  //     .then(() => {
-  //       //response 응답으로 들어온 게시글의 id를 이용해서
-  //       //상세보기로 바로 점프도 가넝이야~~
-  //       router.push({ name: "videoList" });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
-
   return {
     videoList,
     getvideoList,
