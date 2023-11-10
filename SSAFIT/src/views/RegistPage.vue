bcb047dca6f13570d2a30b59eedea1e534903fb7
seom98 <seo_m98@naver.com>
2023-11-10 11:20:29 +0900

    회원가입 만듦

--- SSAFIT/src/views/RegistPage.vue
+++ SSAFIT/src/views/RegistPage.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        <h2>회원가입페이지임.</h2>
+    <div class="regist-form">
+        <form action="" class="form">
+            <h1 class="form__title">regist</h1>
+
+            <div class="regist-div">
+                <input type="text" class="regist-input" placeholder=" ">
+                <label for="" class="regist-label">ID</label>
+            </div>
+
+            <div class="regist-div">
+                <input type="password" class="regist-input" placeholder=" ">
+                <label for="" class="regist-label">Password</label>
+            </div>
+
+            <div class="regist-div">
+                <input type="password" class="regist-input" placeholder=" ">
+                <label for="" class="regist-label">Password확인</label>
+            </div>
+
+            <input type="submit" class="regist-button" value="regist">
+        </form>
     </div>
 </template>
 
@@ -8,4 +27,94 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.regist-form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
+}
+
+.form {
+    width: 360px;
+    padding: 3rem 2rem;
+    border-radius: 1rem;
+    /* box-shadow: 0 10px 25px rgba(92, 99, 105, 0.2); */
+}
+
+.form__title {
+    font-weight: 500;
+    margin-bottom: 2.5rem;
+}
+
+.regist-div {
+    position: relative;
+    height: 52px;
+    margin-bottom: 1.5rem;
+}
+
+.regist-input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 90%;
+    height: 50%;
+    font-size: 1rem;
+    border: 2px solid #DADCE0;
+    border-radius: 0.5rem;
+    outline: none;
+    padding: 1rem;
+    background: none;
+    z-index: 1;
+}
+
+.regist-label {
+    position: absolute;
+    left: 1rem;
+    top: 1.3rem;
+    padding: 0 0.25rem;
+    background-color: #fff;
+    color: #80868B;
+    font-size: 1rem;
+    transition: 0.3s;
+}
+
+.regist-button {
+    display: block;
+    margin-left: auto;
+    padding: 0.75rem 2rem;
+    outline: none;
+    border: none;
+    background-color: rgb(184, 225, 211);
+    color: #fff;
+    font-size: 1rem;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: 0.3s;
+}
+
+.regist-button:hover {
+    background-color: rgb(55, 182, 140);
+}
+
+.regist-input:focus+.regist-label {
+    top: -0.3rem;
+    left: 0.8rem;
+    color: rgb(55, 182, 140);
+    font-size: .75rem;
+    font-weight: 500;
+    z-index: 10;
+}
+
+.regist-input:not(:placeholder-shown).regist-input:not(:focus)+.regist-label {
+    top: -0.3rem;
+    left: 0.8rem;
+    z-index: 10;
+    font-size: .75rem;
+    font-weight: 500;
+}
+
+.regist-input:focus {
+    border: 2px solid rgb(55, 182, 140)
+}
+</style>
